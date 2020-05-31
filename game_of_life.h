20a6ea46688cc5518c8c1cc58b36c81498c65b57
Kostas Chatzopoulos <kwstarikanos@gmail.com>
2020-05-31 20:43:30 +0300

    reformat code

--- game_of_life.h
+++ game_of_life.h
@@ -1,9 +1,12 @@
 #ifndef __GAME_OF_LIFE_H__
 #define __GAME_OF_LIFE_H__
 
-int** create(int n, int m);
-void initialize(int** array, int n, int m);
-void print_array(int** array, int n, int m);
-int operate(int** array, int n, int m);
+int **create(int n, int m);
+
+void initialize(int **array, int n, int m);
+
+void print_array(int **array, int n, int m);
+
+int operate(int **array, int n, int m);
 
 #endif
--- main.c
+++ main.c
@@ -5,19 +5,19 @@
 #define N 10;
 #define M 10;
 
-int main(int argc, char** argv){
-  int n = N;
-  int m = M;
-  
-  int** array = create(n, m);
+int main(int argc, char **argv) {
+    int n = N;
+    int m = M;
 
-  print_array(array, n, m);
-  printf("\n");
-  initialize(array, n, m);
+    int **array = create(n, m);
 
-  /*print initialized array*/
-  
-  print_array(array, n, m);
+    print_array(array, n, m);
+    printf("\n");
+    initialize(array, n, m);
 
-  operate( array, n, m);
+    /*print initialized array*/
+
+    print_array(array, n, m);
+
+    operate(array, n, m);
 }
