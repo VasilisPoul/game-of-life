97b27efb05fbc73fd815ad0c14d5cf9ea152498e
Kostas Chatzopoulos <kwstarikanos@gmail.com>
2020-09-29 18:39:02 +0300

    code clean-up
    Co-authored-by: vasilisp100 <vasilis.p.100@gmail.com>

--- cuda/main.cu
+++ cuda/main.cu
@@ -13,8 +13,8 @@
 char **allocate2DArray(int rows, int columns) {
     char **block;
     int i;
-    block = (char **)malloc(rows * sizeof(char *));
-    block[0] = (char *)malloc(rows * columns * sizeof(char));
+    block = (char **) malloc(rows * sizeof(char *));
+    block[0] = (char *) malloc(rows * columns * sizeof(char));
     for (i = 1; i < rows; i++) {
         block[i] = &(block[0][i * rows]);
     }
@@ -26,7 +26,6 @@ void print_array(char **array, int n) {
     printf("\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-
             printf("%s ", array[i][j] == '1' ? "\u2B1C" : "\u2B1B");
         }
         printf("\n");
@@ -36,80 +35,64 @@ void print_array(char **array, int n) {
 
 
 // Device code
-__global__ void kernel(char* device_old){
-    int	i = blockIdx.x * blockDim.x + threadIdx.x;
-    
+__global__ void kernel(char *device_old) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
     int ix = blockIdx.x * blockDim.x + threadIdx.x;
     int iy = blockIdx.y * blockDim.y + threadIdx.y;
     int idx = iy * N + ix;
-
     printf("%d %d t[%d][%d] - t[%d][%d]=%d\n", blockIdx.x, blockDim.x, threadIdx.x, threadIdx.y, ix, iy, idx);
     __shared__ char local[N];
-
     local[threadIdx.x] = device_old[i];
-
-
-
-
     device_old[i] = 'A';
-
 }
 
 //Host Code
 int main() {
-    char** host_array = NULL, **host_current = NULL, *device_old, *device_current, *temp = NULL;;
+    char **host_array = NULL, **host_current = NULL, *device_old, *device_current, *temp = NULL;;
     size_t pitch;
     int i = 0, j = 0, fd = 0;
-    dim3 m(M,M); 
-    dim3 n((N + (float) M - 1)/(float) M,(N + (float) M - 1)/(float) M); 
+    dim3 m(M, M);
+    dim3 n((N + (float) M - 1) / (float) M, (N + (float) M - 1) / (float) M);
 
     // Array allocations
     host_array = allocate2DArray(N, N);
-    
-
     print_array(host_array, N);
 
     // Read file
     fd = open(FILE_NAME, O_RDONLY);
-    if(fd < 0){
+    if (fd < 0) {
         fprintf(stderr, "Could not open file \"%s\"\n", "320file.txt");
         return -1;
     }
-    while(read(fd, host_array[i++], N));
+    while (read(fd, host_array[i++], N));
     close(fd);
 
     print_array(host_array, N);
 
     // Send 2D to the Device
-    cudaMalloc((void**)&device_old, N * N * sizeof(char));
+    cudaMalloc((void **) &device_old, N * N * sizeof(char));
     cudaMemcpy(device_old, host_array[0], sizeof(char) * N * N, cudaMemcpyHostToDevice);
-   
-    cudaMalloc((void**)&device_current, N * N * sizeof(char));
+
+    cudaMalloc((void **) &device_current, N * N * sizeof(char));
     cudaMemset(device_current, '0', sizeof(char) * N * N);
-   
 
     //TODO: check this
     cudaDeviceSynchronize();
- 
+
     // Computations
-    for ( i = 0; i < STEPS; i++){
+    for (i = 0; i < STEPS; i++) {
 
         //call device function
-       // kernel<<<N, N>>>(device_old/*, device_current*/);
-          
+        // kernel<<<N, N>>>(device_old/*, device_current*/);
+
         // Swap device_old and device_current
         temp = device_old;
         device_old = device_current;
         device_current = temp;
-        
+
 
     }
-
-
     cudaMemcpy(host_array[0], device_current, sizeof(char) * N * N, cudaMemcpyDeviceToHost);
-
     print_array(host_array, N);
-
-    
     return 0;
 }
