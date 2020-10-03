ffbe28e1394a7c93fec8635a4f25c19ba3949c58
vasilisp100 <vasilis.p.100@gmail.com>
2020-10-03 20:19:03 +0300

    cuda:fill internal blocks
    Co-authored-by: Kostas Chatzopoulos <kwstarikanos@gmail.com>

--- cuda/main.cu
+++ cuda/main.cu
@@ -93,99 +93,148 @@ __global__ void kernel(int *old, int *current) {
 
     unsigned int ix = blockIdx.x * (blockDim.x) + threadIdx.x;
     unsigned int iy = blockIdx.y * (blockDim.y) + threadIdx.y;
-    unsigned int idx = (ix + 1) * (N + 2) + (iy + 1);
+    unsigned int idx = ix * N + iy;
 
     // if (blockIdx.x == 1 && blockIdx.y == 1 )
     old[idx] = idx;
-    //lcoal[idx] = idx;
+    // //lcoal[idx] = idx;
 
-    __syncthreads();
-
-
+    // __syncthreads();
 
 
-    // Up
-    if (blockIdx.x == 0 && threadIdx.x == 0) {
-        old[idx - N - 2] = old[idx + N - 2 + N * N];
-    }
-
-    // Left
-    if (blockIdx.y == 0 && threadIdx.y == 0) {
-        old[idx - 1] = old[idx + N - 1];
-    }
-
-    // Right
-    if (blockIdx.y == gridDim.y - 1 && threadIdx.y == blockDim.y - 1) {
-        old[idx + 1] = old[idx - N + 1];
-    }
-
-    // Up right
-    if (blockIdx.y == gridDim.y - 1 && blockIdx.x == 0 && threadIdx.x == 0 && threadIdx.y == blockDim.y - 1) {
-        old[N + 1] = old[(N + 1) * (N + 1)];;
-    }
-
-    // Down
-    if (blockIdx.x == gridDim.x - 1 && threadIdx.x == blockDim.x - 1) {
-        old[idx + N + 2] = old[idx - N + 2 - N * N];
-    }
-
-    // Down right
-    if (blockIdx.y == gridDim.y - 1 && blockIdx.x == gridDim.x - 1 && threadIdx.x == blockDim.x - 1 &&
-        threadIdx.y == blockDim.y - 1) {
-        old[(N + 2) * (N + 1) + N + 1] = old[N + 2 + 1];
-    }
-
-    // Down left
-    if (blockIdx.y == 0 && blockIdx.x == gridDim.x - 1 && threadIdx.x == blockDim.x - 1 && threadIdx.y == 0) {
-        old[(N + 2) * (N + 1)] = old[N + 2 + N];
-    }
-
-    // // Up left
-    if (blockIdx.y == 0 && blockIdx.x == 0 && threadIdx.x == 0 && threadIdx.y == 0) {
-        old[idx - N - 3] = old[(N + 1) * (N + 1) + N - 1];
-    }
+    //split internals
+    if(blockIdx.x > 0 && blockIdx.x < gridDim.x -1){
+        if(blockIdx.y > 0 && blockIdx.y < gridDim.y - 1){
 
 
+            if (blockIdx.x == 1 && blockIdx.y == 1) {
+                    // old[idx] = idx;
+                    local[local_row + 1][local_col+1] = idx;
+                    __syncthreads();
+            
+                    //up
+                    if (local_row == 0){
+                        
+                        local[local_row][local_col+1] = old[idx - N];
+                    }
+                    //down
+                    if (local_row == blockDim.x-2){
+                        
+                        local[local_row+3][local_col+1] = old[idx + 2*N];
+                    }
+                    //left
+                    if (local_col == 0){
+                        local[local_row+1][local_col] = old[idx -1];
+                    }
+                    //right
+                    if (local_col == blockDim.y-2){
+                        local[local_row+1][local_col+3] = old[idx +2];
+                    }
+                    //up left
+                    if (local_col == 0 && local_row == 0){
+                        local[local_row][local_col] = old[idx -N -1];
+                    }
+                    //up right
+                    if (local_col == blockDim.y-2 && local_row == 0){
+                        local[local_row][local_col+3] = old[idx -N +2];
+                    }
+                    //down left
+                    if (local_col == 0 && local_row == blockDim.y -2){
+                        local[local_row+3][local_col] = old[idx +2*N -1];
+                        // printf("local[%d][%d]:\n", local_row+3, local_col);
+                        // printf("old[idx +N -1]: %d\n", old[idx +2*N -1]);
+                    }
+                    //down right
+                    if (local_col == blockDim.y-2 && local_row == blockDim.x -2){
+                        local[local_row+3][local_col+3] = old[idx +2*N +2];
+                        // printf("local[%d][%d]:\n", local_row+3, local_col+3);
+                        // printf("old[idx +N -1]: %d\n", old[idx +2*N +2]);
+                    }
+                    
 
-
-    if (blockIdx.x == 0 && blockIdx.y == 0) {
-        old[idx] = idx;
-        local[local_row + 1][local_col + 1] = idx;
-        
-        __syncthreads();
-
-
-        // Up
-        if (local_row == 0) {
-            local[local_row][local_col + 1] = old[idx + N - 2 + N * N];
-        }
-
-        printf("BLOCKDIM x: %d\n", blockDim.x);
-
-        // // Down
-        // if (local_row == blockDim.x - 1) {
-        //     local[local_row][local_col + 1] = old[idx - N + 2 - N * N];
-        // }
-
-
-
-        if (ix == 0 && iy == 0) {
-
-            for (int i = 0; i < M + 2; i++) {
-                for (int j = 0; j < M + 2; j++) {
-                    printf("%5.4d ", local[i][j]);
+                    //print block
+                    if (ix == 6 && iy == 6) {
+            
+                        for (int i = 0; i < M + 2; i++) {
+                            for (int j = 0; j < M + 2; j++) {
+                                printf("%5.4d ", local[i][j]);
+                            }
+                            printf("\n");
+                        }
+                        printf("\n");
+                    }
                 }
-                printf("\n");
-            }
-            printf("\n");
+            
         }
     }
+   
+
+    // if (blockIdx.x == 0 && blockIdx.y == 0) {
+    //     // old[idx] = idx;
+    //     local[local_row + 1][local_col+1] = idx;
+        
+    //     __syncthreads();
+
+    //     //print block
+    //     if (ix == 0 && iy == 0) {
+
+    //         for (int i = 0; i < M + 2; i++) {
+    //             for (int j = 0; j < M + 2; j++) {
+    //                 printf("%5.4d ", local[i][j]);
+    //             }
+    //             printf("\n");
+    //         }
+    //         printf("\n");
+    //     }
+    // }
 
     __syncthreads();
 
 
 
 
+
+
+//  // Up
+//  if (blockIdx.x == 0 && threadIdx.x == 0) {
+//     old[idx - N - 2] = old[idx + N - 2 + N * N];
+// }
+
+// // Left
+// if (blockIdx.y == 0 && threadIdx.y == 0) {
+//     old[idx - 1] = old[idx + N - 1];
+// }
+
+// // Right
+// if (blockIdx.y == gridDim.y - 1 && threadIdx.y == blockDim.y - 1) {
+//     old[idx + 1] = old[idx - N + 1];
+// }
+
+// // Up right
+// if (blockIdx.y == gridDim.y - 1 && blockIdx.x == 0 && threadIdx.x == 0 && threadIdx.y == blockDim.y - 1) {
+//     old[N + 1] = old[(N + 1) * (N + 1)];;
+// }
+
+// // Down
+// if (blockIdx.x == gridDim.x - 1 && threadIdx.x == blockDim.x - 1) {
+//     old[idx + N + 2] = old[idx - N + 2 - N * N];
+// }
+
+// // Down right
+// if (blockIdx.y == gridDim.y - 1 && blockIdx.x == gridDim.x - 1 && threadIdx.x == blockDim.x - 1 &&
+//     threadIdx.y == blockDim.y - 1) {
+//     old[(N + 2) * (N + 1) + N + 1] = old[N + 2 + 1];
+// }
+
+// // Down left
+// if (blockIdx.y == 0 && blockIdx.x == gridDim.x - 1 && threadIdx.x == blockDim.x - 1 && threadIdx.y == 0) {
+//     old[(N + 2) * (N + 1)] = old[N + 2 + N];
+// }
+
+// // // Up left
+// if (blockIdx.y == 0 && blockIdx.x == 0 && threadIdx.x == 0 && threadIdx.y == 0) {
+//     old[idx - N - 3] = old[(N + 1) * (N + 1) + N - 1];
+// }
 
 
 
@@ -213,7 +262,7 @@ int main() {
     int i = 0, fd = 0;
 
     // Threads (2D) per block
-    dim3 m(M+2, M+2);
+    dim3 m(M, M);
 
     // Blocks (2D grid)
     dim3 n((unsigned int) ((N + (float) M - 1) / (float) M), (unsigned int) ((N + (float) M - 1) / (float) M));
@@ -221,7 +270,7 @@ int main() {
     //assert(N == M * M * 2);
 
     // Array allocations
-    host_array = allocate2DArray(N + 2, N + 2);
+    host_array = allocate2DArray(N, N);
 
 //    // Read file
 //    if ((fd = open(FILE_NAME, O_RDONLY)) < 0) {
@@ -233,19 +282,19 @@ int main() {
 //    close(fd);
 
     printf("host_array before:\n");
-    print_array(host_array, true, true, N + 2, N + 2, N + 2, N + 2);
+    print_array(host_array, true, true, N, N, N, N);
 
     // Initialize 2D 'old' array on device
-    cudaMalloc((void **) &device_old, (N + 2) * (N + 2) * sizeof(int));
+    cudaMalloc((void **) &device_old, N * N * sizeof(int));
 
     // Copy 2D 'old' array on device
-    cudaMemcpy(device_old, host_array[0], (N + 2) * (N + 2) * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_old, host_array[0], N * N * sizeof(int), cudaMemcpyHostToDevice);
 
     // Initialize 2D 'current' array on device
-    cudaMalloc((void **) &device_current, (N + 2) * (N + 2) * sizeof(int));
+    cudaMalloc((void **) &device_current, N * N * sizeof(int));
 
     // Copy 2D 'current' array on device
-    cudaMemset(device_current, '0', (N + 2) * (N + 2) * sizeof(int));
+    cudaMemset(device_current, '0', N * N * sizeof(int));
 
 
     // Computations
@@ -255,10 +304,10 @@ int main() {
         kernel<<<n, m>>>(device_old, device_current);
 
         // Copy 2D 'device_current' array on host
-        cudaMemcpy(host_array[0], device_old, sizeof(int) * (N + 2) * (N + 2), cudaMemcpyDeviceToHost);
+        cudaMemcpy(host_array[0], device_old, sizeof(int) * N * N, cudaMemcpyDeviceToHost);
 
         printf("host_array on step %d:\n", i);
-        print_array(host_array, true, true, N + 2, N + 2, N + 2, N + 2);
+        print_array(host_array, true, true, N, N, N, N);
 
         // Swap 'device_old' and 'device_current' arrays
         temp = device_old;
